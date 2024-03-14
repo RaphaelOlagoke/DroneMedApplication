a6a4b4a1fab374068c7e3dd8730f877e408dede4
Raphael <raphael@Raphaels-MacBook-Air.local>
2024-03-14 10:58:57 +0100

    initial commit

---
+++ src/main/kotlin/com/demo/DroneMed/DroneMedApplication.kt
@@ -0,0 +1,11 @@
+package com.demo.DroneMed
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DroneMedApplication
+
+fun main(args: Array<String>) {
+	runApplication<DroneMedApplication>(*args)
+}
---
+++ src/test/kotlin/com/demo/DroneMed/DroneMedApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.demo.DroneMed
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DroneMedApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
